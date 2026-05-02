c25081ecdcaa6483874176920f6bf1dbf3ed24ac
Jihed <jihed.gouay@gmail.com>
2026-05-02 21:42:14 +0100

    mounting the vue app

--- job-search/src/main.js
+++ job-search/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import Hello from "./Hello.vue";
 
-createApp(App).mount('#app')
+createApp(Hello).mount('#app');
