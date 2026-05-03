5501d03faa63bbbb20f43e9ee27b58516ec3cb35
Jihed <jihed.gouay@gmail.com>
2026-05-03 13:25:38 +0100

    targeted html element

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -3,6 +3,15 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-
+    <div class="wrapper">
+        <h1>VueJS</h1>
+<HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+    
 </template>
 
+<style>
+h1 {
+    color: #42b983;
+}
+</style>
